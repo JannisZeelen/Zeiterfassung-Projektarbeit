e78745aef55d8ad093aa53049c8f9817f41062de
zanyyy <janzee@hotmail.de>
2023-12-22 18:45:12 +0100

    - Aufspaltung von show_plot Funktion - Kommentare auf Deutsch geändert - Matplotlib Style geändert um Bar + Pie Diagramm hinzufügen zu können - Pie Diagramm Subplot erstellt - TODO's abgearbeitet / erweitert

--- projektarbeit.py
+++ projektarbeit.py
@@ -5,10 +5,11 @@ import csv
 import matplotlib.pyplot as plt
 import sv_ttk
 
+# Dateipfad für die Zeiterfassungsdaten
 file = 'zeiterfassung.csv'
 
 
-# ======= datetime functions =======
+# ======= Datetime Funktionen =======
 def get_date():
     current_datetime = dt.now()
     return current_datetime.strftime("%d.%m.%Y")
@@ -23,6 +24,7 @@ def get_time():
 
 
 def get_worktime_float(time):
+    # FFunktion, um die Arbeitszeit in Stunden als Float zu erhalten
     formatted_time = dt.strptime(time, "%H:%M:%S")
     total_seconds = td(hours=formatted_time.hour, minutes=formatted_time.minute,
                        seconds=formatted_time.second).total_seconds()
@@ -30,15 +32,17 @@ def get_worktime_float(time):
     return worktime_float
 
 
-# ======= button functions =======
+# ======= Button-Funktionen =======
 def check_in():
-    print(f"Checking you in!")
+    # Check-in Button deaktivieren, Check-out Button aktivieren, Aufrufen von Check-in Funktion
+    print(f"Checking you in!")  # TODO remove this?
     button_check_in["state"] = "disabled"
     button_check_out["state"] = "active"
     csv_write_check_in()
 
 
 def check_out():
+    # Check-out Button deaktivieren, Check-in Button aktivieren, Aufrufen von Check-out Funktion
     print(f"Checking you out!")
     button_check_out["state"] = "disabled"
     button_check_in["state"] = "active"
@@ -46,11 +50,13 @@ def check_out():
 
 
 def user_quit():
+    # Tkinter schließen
     root.quit()
 
 
-# ======= .csv functions =======
+# ======= .csv Funktionen =======
 def get_status():
+    # Überprüfung, ob bereits eingecheckt worden ist, dementsprechene Button De-/Aktivierung
     with open(file, 'r', newline='') as csv_file:
         existing_data = list(csv.reader(csv_file))
 
@@ -60,6 +66,7 @@ def get_status():
 
 
 def csv_write_check_in():
+    # Datum + Check-in Zeit in neue Zeile schreiben
     with open(file, 'a', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow([get_date(), get_time()])
@@ -67,50 +74,76 @@ def csv_write_check_in():
 
 
 def csv_write_check_out():
+    # Check-out Zeit + Arbeitszeit in bestehende Zeile schreiben
     with open(file, 'r+', newline='') as csv_file:
+        # Bestehende Daten in Variable zwischenspeichern
         existing_data = list(csv.reader(csv_file))
 
+        # Überprüfung, ob bereits eingecheckt worde, falls ja Check-in Zeit speichern
         if existing_data and len(existing_data[-1]) == 2:
             check_in_time = dt.strptime(existing_data[-1][1], "%H:%M:%S")
             check_out_time = dt.strptime(get_time(), "%H:%M:%S")
 
-            timedelta = check_out_time - check_in_time
+            # Berechne die Arbeitszeit für den aktuellen Tag
             base_date = dt(1900, 1, 1)
+            timedelta = check_out_time - check_in_time
             worktime = base_date + timedelta
-            print(worktime)
-            # worktime_float = get_worktime_float(worktime.strftime("%H:%M:%S"))
+
+            # Erweitern der Check-out und Arbeitszeit
             existing_data[-1].extend([get_time(), worktime.strftime("%H:%M:%S")])
 
-            # Move the file cursor to the beginning to overwrite the existing data
+            # An Beginn der Datei springen und neue Daten schreiben
             csv_file.seek(0)
             csv_writer = csv.writer(csv_file)
-
-            # Write the modified data back to the file
             csv_writer.writerows(existing_data)
 
     print('Zeiten wurden gespeichert')
 
 
-def show_plot():
-    # TODO: Funktion derzeit: Bar plot der letzten 5 tage float stunden arbeitszeit. / andere ideen besser?
+def create_plot(work_hours, last_5_days, last_5_days_no_year):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+    # Bar Plot
+    ax1.bar(last_5_days_no_year, work_hours, color='skyblue')
+    ax1.set_xlabel('Wochentag')
+    ax1.set_ylabel('Gesamtarbeitsstunden')
+    ax1.set_title('Arbeitszeiten der letzten 5 Tage')
+
+    # Pie Plot in %
+    ax2.pie(work_hours, labels=last_5_days, autopct='%1.1f%%', startangle=90,
+            colors=['skyblue', 'lightcoral', 'lightgreen', 'gold', 'lightpink'])
+    ax2.set_title('Aufteilung Arbeitszeiten letzten 5 Tage')
+    plt.tight_layout()
+
+
+def math_plot():
+    # TODO: Funktion derzeit: Bar plot der letzten 5 tage float stunden arbeitszeit. / andere ideen besser? mehrere plots
     with open('zeiterfassung.csv', 'r') as csv_file:
         reader = csv.reader(csv_file)
-        rows = list(reader)[-5:]
+        rows = list(reader)
 
-    work_hours = []
-    days = []
+    # Leeres Dictionary für zukünftige Date:Worktime Werte
+    work_hours_per_day = {}
 
-    for row in rows:
-        work_hours.append(get_worktime_float(row[3]))
-        days.append(row[0])
+    # Datum und Arbeitszeit in Listen speichern
+    for row in rows[1:]:
+        date = row[0]
+        work_hours = get_worktime_float(row[3])
 
-    # work_hours.reverse()
-    # days.reverse()
+        # Wenn der Tag bereits im Dictionary ist, addiere die Arbeitszeit
+        if date in work_hours_per_day:
+            work_hours_per_day[date] += work_hours
+        else:
+            work_hours_per_day[date] = work_hours
 
-    plt.bar(days, work_hours, color='skyblue')
-    plt.xlabel('Wochentag')
-    plt.ylabel('Arbeitsstunden')
-    plt.title('Arbeitszeiten letzte 5 Tage')
+    # Bereitstellen von Listen für die Ploterstellung
+    last_5_days = list(work_hours_per_day.keys())[-5:]
+    last_5_days_no_year = [day[:-4] for day in last_5_days]
+    work_hours = [work_hours_per_day[day] for day in last_5_days]
+
+    # Funktion zum Erstellen der Plots aufrufen
+    create_plot(work_hours, last_5_days, last_5_days_no_year)
+
     plt.show()
 
 
@@ -144,7 +177,7 @@ button_check_out = ttk.Button(text='Check out', command=check_out, state="disabl
 if get_status():
     button_check_out['state'] = "normal"
 
-button_plot = ttk.Button(text='Show working times', command=show_plot)
+button_plot = ttk.Button(text='Show working times', command=math_plot)
 button_user_quit = ttk.Button(text='Quit', command=user_quit)
 
 button_plot.grid(row=2, column=0, columnspan=2, sticky='nsew')
@@ -166,6 +199,6 @@ root.mainloop()  # Eventloop starten
 # TODO make connection to github
 # TODO Pause button und abziehen von delta
 # TODO Bild von alfa logo
-# TODO alles auf ttk ändern
-# TODO Berechnung von timedelta mit tagen einbeziehen sonst bei über 24h von null
 # TODO Project vorstellen: 15 Minuten Zeit
+# TODO überprüfung wenn erst am nächsten tag ausgecheckt wird(vorheriger tag bis 24 dann neuer eintrag?
+# TODO Alles erklären könnnen
